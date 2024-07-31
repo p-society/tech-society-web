f4dd5ffaf0826941409cf7fe66e92a2b2e0bd538
Maheswar Mahapatra <maheswar2110@gmail.com>
2024-07-31 11:20:18 +0530

    update

--- src/components/Team/TeamData.js
+++ src/components/Team/TeamData.js
@@ -51,7 +51,58 @@ const TeamData = [
     },
   },
   {
+    teamName: "Game Development",
+    members: {
+        member1:{
+            img: "/Leaders/hero1.jpeg",
+            name: 'John Doe'
+        },
+        member2:{
+            img: "/Leaders/hero1.jpeg",
+            name: 'John Doe'
+        },
+        member3:{
+            img: "/Leaders/hero1.jpeg",
+            name: 'John Doe'
+        },
+    },
+  },
+  {
     teamName: "Cloud",
+    members: {
+        member1:{
+            img: "/Leaders/hero1.jpeg",
+            name: 'Saswat Parasar Behera'
+        },
+        member2:{
+            img: "/Leaders/hero1.jpeg",
+            name: 'John Doe'
+        },
+        member3:{
+            img: "/Leaders/hero1.jpeg",
+            name: 'John Doe'
+        },
+    },
+  },
+  {
+    teamName: "App Development",
+    members: {
+        member1:{
+            img: "/Leaders/hero1.jpeg",
+            name: 'Saswat Parasar Behera'
+        },
+        member2:{
+            img: "/Leaders/hero1.jpeg",
+            name: 'John Doe'
+        },
+        member3:{
+            img: "/Leaders/hero1.jpeg",
+            name: 'John Doe'
+        },
+    },
+  },
+  {
+    teamName: "Design",
     members: {
         member1:{
             img: "/Leaders/hero1.jpeg",
