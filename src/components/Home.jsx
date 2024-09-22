9d042a3a672bbd65abd4392edd617b7970e609aa
Manish Biswal <146447671+iamanishx@users.noreply.github.com>
2024-09-23 00:56:54 +0530

    gallery.jsx update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,21 +6,21 @@ const Home = () => {
     return (
         <>
             <div className="mb">
-         
-                
-             
+
+
+
             </div>
             <div className="fd">
-                    <h2>TechSoc IIIT BHUBANESWAR: </h2>
-                    <p>
-                        The main div uses display: flex to align both child divs (left and right)
-                        horizontally. `justify-content: space-between` ensures the left div is aligned
-                        to the left and the right div to the right. `align-items: flex-start` aligns
-                        both divs at the top, but you can manually shift the right div lower by adding
-                        margin-top to it.
-                    </p>
-                    <button className='jd'>Know More</button>
-                </div>
+                <h2 className='mx-auto mt-0 text-center leading-relaxed text-yellow-600 text-weight-900 text-3xl font-bold  '>WHAT IS TECH-SOCIETY? </h2>
+                <p className='mx-auto mt-0 max-w-5xl text-center leading-relaxed text-black-500 dark:text-black-900 text-xl'>
+                    The main div uses display: flex to align both child divs (left and right)
+                    horizontally. `justify-content: space-between` ensures the left div is aligned
+                    to the left and the right div to the right. `align-items: flex-start` aligns
+                    both divs at the top, but you can manually shift the right div lower by adding
+                    margin-top to it.
+                </p>
+                <button className='jd'>Know More</button>
+            </div>
         </>
     );
 };
