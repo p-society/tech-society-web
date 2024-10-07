9d6c32db68a1ae9af07bb8d4d7c28a87c7b8d44e
Soubhik Gon <119437069+zakhaev26@users.noreply.github.com>
2024-10-07 15:09:05 +0530

    chore(code-qual): remove logs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -28,12 +28,7 @@ const Contact = () => {
       reply_to: formData.email,
       message: formData.message,
     };
-
-    console.log(import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-      templateParams,
-      import.meta.env.VITE_APP_EMAILJS_USER_ID)
-
+    
     emailjs.send(
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
       import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
