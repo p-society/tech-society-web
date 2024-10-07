5cd94cec5babd6afc61cb49d44dfae42c2d5c0fd
zakhaev26 <b422056@iiit-bh.ac.in>
2024-10-07 15:07:16 +0530

    feat: `emailjs` integrations

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import Footer from './components/footer';
 import Contact from './components/Contact';
 import About from './components/About';
 // import Gallery from './components/Gallery';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Test from './components/Test'
 import './App.css'
 
@@ -13,10 +16,11 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <ToastContainer />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/community" element={<Community />} />
-        <Route path="/Gallery" element={<Test/>} />
+        <Route path="/Gallery" element={<Test />} />
         <Route path="/About" element={<About />} />
         <Route path="/Contact" element={<Contact />} />
       </Routes>
