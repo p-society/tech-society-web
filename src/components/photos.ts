a5e1eca13467b09bc0de60a6c31ada51f1df575d
zakhaev26 <b422056@iiit-bh.ac.in>
2024-10-12 13:39:43 +0530

    feat: add gallery images

--- src/components/photos.ts
+++ src/components/photos.ts
@@ -1,38 +1,70 @@
+// @ts-expect-error
+import g1 from "../assets/gallery/1.png";
+import g2 from "../assets/gallery/2.png";
+import g3 from "../assets/gallery/3.png";
+import g4 from "../assets/gallery/4.png";
+import g5 from "../assets/gallery/5.png";
+import g6 from "../assets/gallery/6.png";
+import g7 from "../assets/gallery/7.png";
+import g8 from "../assets/gallery/8.png";
+import g9 from "../assets/gallery/9.png";
+import g10 from "../assets/gallery/10.png";
+import g12 from "../assets/gallery/12.png";
 export const photos = [
   {
-    src: 'https://plus.unsplash.com/premium_photo-1683865776032-07bf70b0add1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXJsfGVufDB8fDB8fHww',
-    width: getRandomWidth(),
-    height: getRandomHeight(4 / 3)
+    src: g1,
+    width: 600,
+    height: 400,
   },
   {
-    src: 'https://plus.unsplash.com/premium_photo-1683865776032-07bf70b0add1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXJsfGVufDB8fDB8fHww',
-    width: getRandomWidth(),
-    height: getRandomHeight(4 / 3)
+    src: g2,
+    width: 600,
+    height: 400,
   },
   {
-    src: 'https://plus.unsplash.com/premium_photo-1683865776032-07bf70b0add1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXJsfGVufDB8fDB8fHww',
-    width: getRandomWidth(),
-    height: getRandomHeight(4 / 3)
-  },  {
-    src: 'https://plus.unsplash.com/premium_photo-1683865776032-07bf70b0add1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXJsfGVufDB8fDB8fHww',
-    width: getRandomWidth(),
-    height: getRandomHeight(4 / 3)
-  },  {
-    src: 'https://plus.unsplash.com/premium_photo-1683865776032-07bf70b0add1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXJsfGVufDB8fDB8fHww',
-    width: getRandomWidth(),
-    height: getRandomHeight(4 / 3)
-  },  {
-    src: 'https://plus.unsplash.com/premium_photo-1683865776032-07bf70b0add1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXJsfGVufDB8fDB8fHww',
-    width: getRandomWidth(),
-    height: getRandomHeight(4 / 3)
-  },  {
-    src: 'https://plus.unsplash.com/premium_photo-1683865776032-07bf70b0add1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXJsfGVufDB8fDB8fHww',
-    width: getRandomWidth(),
-    height: getRandomHeight(4 / 3)
-  },  {
-    src: 'https://plus.unsplash.com/premium_photo-1683865776032-07bf70b0add1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXJsfGVufDB8fDB8fHww',
-    width: getRandomWidth(),
-    height: getRandomHeight(4 / 3)
+    src: g3,
+    width: 600,
+    height: 400,
+  },
+  {
+    src: g4,
+    width: 600,
+    height: 400,
+  },
+  {
+    src: g5,
+    width: 600,
+    height: 400,
+  },
+  {
+    src: g6,
+    width: 600,
+    height: 400,
+  },
+  {
+    src: g7,
+    width: 600,
+    height: 400,
+  },
+  {
+    src: g8,
+    width: 600,
+    height: 400,
+  },
+  {
+    src: g9,
+    width: 600,
+    height: 400,
+  },
+  {
+    src: g10,
+    width: 600,
+    height: 400,
+  },
+  {
+    src: g12,
+    width: 600,
+    height: 400,
   },
 ];
 
@@ -43,5 +75,5 @@ function getRandomWidth(maxWidth = 900) {
 
 function getRandomHeight(aspectRatio) {
   const width = getRandomWidth();
-  return Math.floor(width / aspectRatio); 
+  return Math.floor(width / aspectRatio);
 }
