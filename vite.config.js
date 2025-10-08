bfd40c939349bd8c34bca51c6c627fee6442c25f
ujsquared <ujsquared@gmail.com>
2025-10-08 08:18:50 +0530

    fix: SEO optimization

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      sitemap({
+      hostname: 'https://techsoc-iiitbbsr.com',
+    }),
+  ],
 })
